39ea2a8218b94627efa13ca0836384937ed1bff6
Hugo Ponce <hugoponcequiroz@gmail.com>
2020-04-04 22:17:30 -0500

    Patrones creacionales - Builder 2

--- src/Builder/Main.java
+++ src/Builder/Main.java
@@ -4,12 +4,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		Usuario huan = Usuario.Make("HuAn", "Ponce")
+				.setMedioContacto(true)
 				.setTelefono("0990 654 321")
 				.setDireccion("Ecuador")
 				.setEmail("hugoponcequiro@gmail.com")
 				.Build();
 		
+		Usuario jose = Usuario.Make("Jose", "Alcivar").Build();
+		
 		System.out.println(huan);
+		System.out.println(jose);
 	}
 
 }
--- src/Builder/Usuario.java
+++ src/Builder/Usuario.java
@@ -5,28 +5,25 @@ public class Usuario {
 	private String nombre;
 	private String apellido;
 	
+	private boolean medioContacto;
+	
 	private String email;
 	private String telefono;
 	private String direccion;
 	
-	public Usuario setEmail(String email) {
-		this.email = email;
-		return this;
-	}
-
-	public Usuario setTelefono(String telefono) {
-		this.telefono = telefono;
-		return this;
-	}
-
-	public Usuario setDireccion(String direccion) {
-		this.direccion = direccion;
-		return this;
+	public BuilderUsuario setMedioContacto(boolean medioContacto) {
+		if(!medioContacto) {
+			throw new IllegalArgumentException(
+					"No es posible asignar un valor falso a medio de contacto");
+		}
+		this.medioContacto = medioContacto;
+		return new BuilderUsuario(this);
 	}
 
 	private Usuario(String nombre, String apellido) {
 		this.nombre = nombre;
 		this.apellido = apellido;
+		this.medioContacto = false;
 		this.email = "";
 		this.telefono = "";
 		this.direccion = "";
@@ -49,4 +46,31 @@ public class Usuario {
 				", direccion=" + this.direccion + "]";
 	}
 
+	public static class BuilderUsuario {
+		private Usuario usuario;
+		
+		public BuilderUsuario(Usuario usuario) {
+			this.usuario = usuario;
+		}
+		
+		public BuilderUsuario setEmail(String email) {
+			this.usuario.email = email;
+			return this;
+		}
+
+		public BuilderUsuario setTelefono(String telefono) {
+			usuario.telefono = telefono;
+			return this;
+		}
+
+		public BuilderUsuario setDireccion(String direccion) {
+			usuario.direccion = direccion;
+			return this;
+		}
+		
+		public Usuario Build() {
+			return usuario;
+		}
+
+	}
 }
